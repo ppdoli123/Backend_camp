e39912781d1c7e3cab376d0a1b845d3df87a84ce
ppdoli123 <ppdoli123@naver.com>
2023-11-14 15:12:14 +0900

    feat(Discountinfo): 주중&주말 할인에 대한 메서드

--- src/main/java/christmas/domain/Discountinfo.java
+++ src/main/java/christmas/domain/Discountinfo.java
@@ -5,9 +5,19 @@ import java.util.List;
 
 public class Discountinfo {
     private int totalOrderAmount;
+    private boolean weekdayDiscount;
+    private boolean weekendDiscount;
+    private boolean specialDiscount;
+    private Menu giftMenu;
+    private List<String> benefits;
 
     public Discountinfo() {
         this.totalOrderAmount = 0;
+        this.weekdayDiscount = false;
+        this.weekendDiscount = false;
+        this.specialDiscount = false;
+        this.giftMenu = null;
+        this.benefits = new ArrayList<>();
     }
 
     public int getTotalOrderAmount() {
@@ -17,4 +27,45 @@ public class Discountinfo {
     public void setTotalOrderAmount(int totalOrderAmount) {
         this.totalOrderAmount = totalOrderAmount;
     }
+
+    public boolean isWeekdayDiscount() {
+        return weekdayDiscount;
+    }
+
+    public void setWeekdayDiscount(boolean weekdayDiscount) {
+        this.weekdayDiscount = weekdayDiscount;
+    }
+
+    public boolean isWeekendDiscount() {
+        return weekendDiscount;
+    }
+
+    public void setWeekendDiscount(boolean weekendDiscount) {
+        this.weekendDiscount = weekendDiscount;
+    }
+
+    public boolean isSpecialDiscount() {
+        return specialDiscount;
+    }
+
+    public void setSpecialDiscount(boolean specialDiscount) {
+        this.specialDiscount = specialDiscount;
+    }
+
+    public Menu getGiftMenu() {
+        return giftMenu;
+    }
+
+    public void setGiftMenu(Menu giftMenu) {
+        this.giftMenu = giftMenu;
+    }
+
+    public List<String> getBenefits() {
+        return benefits;
+    }
+
+    public void addBenefit(String benefit) {
+        this.benefits.add(benefit);
+    }
+
 }
