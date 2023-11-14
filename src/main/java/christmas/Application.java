3a0fa0b6e5410f741d4d85b70e76c0e667c98b66
ppdoli123 <ppdoli123@naver.com>
2023-11-15 00:39:31 +0900

    feat(Application): 입력과 출력 메서드를 합친 메인 메서드 추가

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,34 @@
 package christmas;
 
+import christmas.controller.Controller;
+import christmas.domain.*;
+import christmas.view.Inputview;
+import christmas.view.Outputview;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        Controller controller = new Controller();
+
+        int visitDate = Inputview.getVisitDate();
+
+        List<Menu> menuList = controller.getMenuList();
+        List<Order> menuOrders = Inputview.getMenuOrders(menuList);
+
+        int totalOrderAmount = calculateTotalOrderAmount(menuOrders);
+        DiscountInfo discountInfo = DiscountCalculator.calculateDiscount(visitDate,totalOrderAmount);
+
+        Outputview.printEventPreview(visitDate, menuOrders, discountInfo);
+
+    }
+
+    private static int calculateTotalOrderAmount(List<Order> menuOrders) {
+        int totalOrderAmount = 0;
+        for (Order order : menuOrders) {
+            totalOrderAmount += order.getMenu().getPrice() * order.getQuantity();
+        }
+        return totalOrderAmount;
     }
 }
