7a9f432b7557d4e81cf19c71aaaac19827846c32
ppdoli123 <ppdoli123@naver.com>
2023-11-15 00:15:53 +0900

    feat(DiscountCalculator): 주문 금액, 할인 여부 ,증정 메뉴,혜택 내역을 저장하는 메서드 추가

--- src/main/java/christmas/domain/DiscountCalculator.java
+++ src/main/java/christmas/domain/DiscountCalculator.java
@@ -5,6 +5,53 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DiscountCalculator {
+    public static DiscountInfo calculateDiscount(int visitDate, int totalOrderAmount) {
+        DiscountInfo discountInfo = new DiscountInfo();
+        discountInfo.setTotalOrderAmount(totalOrderAmount);
+
+        // 할인 정보 설정
+        if (visitDate >= 1 && visitDate <= 25) {
+            int discountAmount = (visitDate - 1) * 100;
+            discountInfo.setTotalOrderAmount(totalOrderAmount - discountAmount);
+        }
+
+        if (visitDate >= 1 && visitDate <= 31) {
+            if (isWeekend(visitDate)) {
+                discountInfo.setWeekendDiscount(true);
+            } else {
+                discountInfo.setWeekdayDiscount(true);
+            }
+        }
+
+        if (isSpecialDiscount(visitDate)) {
+            discountInfo.setSpecialDiscount(true);
+        }
+
+        if (isGiftEventQualified(totalOrderAmount)) {
+            Menu giftMenu = new Menu("샴페인", 0);
+            discountInfo.setGiftMenu(giftMenu);
+        }
+
+        // 혜택 정보 설정
+        if (discountInfo.isWeekdayDiscount()) {
+            discountInfo.addBenefit("평일 할인: -2,023원");
+        }
+
+        if (discountInfo.isWeekendDiscount()) {
+            discountInfo.addBenefit("주말 할인: -2,023원");
+        }
+
+        if (discountInfo.isSpecialDiscount()) {
+            discountInfo.addBenefit("특별 할인: -1,000원");
+        }
+
+        if (discountInfo.getGiftMenu() != null) {
+            discountInfo.addBenefit("증정 이벤트: -25,000원");
+        }
+
+        return discountInfo;
+    }
+
     private static boolean isWeekend(int visitDate) {
         return visitDate % 7 == 5 || visitDate % 7 == 6;
     }
