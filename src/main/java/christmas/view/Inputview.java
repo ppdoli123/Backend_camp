9927d132dd97527ebded68929309da60e8ebd330
ppdoli123 <ppdoli123@naver.com>
2023-11-15 00:29:34 +0900

    feat(Inputview): scanner 대신 console.readline으로 수정

--- src/main/java/christmas/view/Inputview.java
+++ src/main/java/christmas/view/Inputview.java
@@ -1,25 +1,24 @@
 package christmas.view;
-
 import christmas.domain.Menu;
 import christmas.domain.Order;
 
+import java.io.Console;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Inputview {
-    private static Scanner scanner = new Scanner(System.in);
+    private static Console console = System.console();
 
     public static int getVisitDate() {
         System.out.print("12월 중 식당 예상 방문 날짜를 입력해주세요: ");
-        return scanner.nextInt();
+        return Integer.parseInt(console.readLine());
     }
 
     public static List<Order> getMenuOrders(List<Menu> menuList) {
         List<Order> menuOrders = new ArrayList<>();
 
         System.out.print("주문하실 메뉴를 메뉴와 개수로 입력해주세요 (e.g. 해산물파스타-2,레드와인-1,초코케이크-1): ");
-        String input = scanner.next();
+        String input = console.readLine();
         String[] menuOrderPairs = input.split(",");
 
         for (String pair : menuOrderPairs) {
