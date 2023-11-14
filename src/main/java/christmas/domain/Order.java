fd6837d70450d7f86ab494cca7f45a6ab4b070da
ppdoli123 <ppdoli123@naver.com>
2023-11-14 14:52:48 +0900

    feat(Order): 주문정보를 담은 클래스 생성

---
+++ src/main/java/christmas/domain/Order.java
@@ -0,0 +1,19 @@
+package christmas.domain;
+
+public class Order {
+    private Menu menu;
+    private int quantity;
+
+    public Order(Menu menu, int quantity) {
+        this.menu = menu;
+        this.quantity = quantity;
+    }
+
+    public Menu getMenu() {
+        return menu;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+}
