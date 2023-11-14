b64cf6aabd1175bdefe106df95ae20ccec83ce74
ppdoli123 <ppdoli123@naver.com>
2023-11-15 00:39:00 +0900

    feat(Inputview): scanner 대신 console.readline으로 수정

--- src/main/java/christmas/view/Inputview.java
+++ src/main/java/christmas/view/Inputview.java
@@ -1,37 +1,54 @@
 package christmas.view;
 import christmas.domain.Menu;
 import christmas.domain.Order;
+import camp.nextstep.edu.missionutils.Console;
 
-import java.io.Console;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Inputview {
-    private static Console console = System.console();
-
     public static int getVisitDate() {
-        System.out.print("12월 중 식당 예상 방문 날짜를 입력해주세요: ");
-        return Integer.parseInt(console.readLine());
+        while (true) {
+            try {
+                System.out.print("12월 중 식당 예상 방문 날짜를 입력해주세요: ");
+                int visitDate = Integer.parseInt(Console.readLine());
+                return visitDate;
+            } catch (NumberFormatException e) {
+                printError("[ERROR] 잘못된 입력입니다. 숫자로 입력해주세요.");
+            }
+        }
     }
 
     public static List<Order> getMenuOrders(List<Menu> menuList) {
         List<Order> menuOrders = new ArrayList<>();
 
-        System.out.print("주문하실 메뉴를 메뉴와 개수로 입력해주세요 (e.g. 해산물파스타-2,레드와인-1,초코케이크-1): ");
-        String input = console.readLine();
-        String[] menuOrderPairs = input.split(",");
+        while (true) {
+            try {
+                System.out.print("주문하실 메뉴를 메뉴와 개수로 입력해주세요 (e.g. 해산물파스타-2,레드와인-1,초코케이크-1): ");
+                String input = Console.readLine();
 
-        for (String pair : menuOrderPairs) {
-            String[] menuOrder = pair.split("-");
-            String menuName = menuOrder[0];
-            int quantity = Integer.parseInt(menuOrder[1]);
+                String[] menuOrderPairs = input.split(",");
+                for (String pair : menuOrderPairs) {
+                    String[] menuOrder = pair.split("-");
+                    String menuName = menuOrder[0];
+                    int quantity = Integer.parseInt(menuOrder[1]);
 
-            Menu menu = findMenuByName(menuList, menuName);
-            Order order = new Order(menu, quantity);
-            menuOrders.add(order);
+                    Menu menu = findMenuByName(menuList, menuName);
+                    if (menu == null) {
+                        throw new IllegalArgumentException("[ERROR] 잘못된 메뉴명입니다: " + menuName);
+                    }
+
+                    Order order = new Order(menu, quantity);
+                    menuOrders.add(order);
+                }
+
+                return menuOrders;
+            } catch (NumberFormatException e) {
+                printError("[ERROR] 잘못된 입력입니다. 숫자로 입력해주세요.");
+            } catch (IllegalArgumentException e) {
+                printError(e.getMessage());
+            }
         }
-
-        return menuOrders;
     }
 
     private static Menu findMenuByName(List<Menu> menuList, String menuName) {
@@ -41,5 +58,9 @@ public class Inputview {
             }
         }
         return null;
+    }
+
+    private static void printError(String message) {
+        System.out.println(message);
     }
 }
